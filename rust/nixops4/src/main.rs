34d654a72e132d74433504ac297424be1dd33a6c
Robert Hensing <robert@roberthensing.nl>
2024-04-08 18:36:33 +0200

    feat: nixops4 CLI skeleton with clap

--- rust/nixops4/src/main.rs
+++ rust/nixops4/src/main.rs
@@ -1,6 +1,25 @@
-use nixops4_core;
+use clap::Command;
+use std::process::exit;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn root_command() -> Command {
+    Command::new("nixops4")
+        .version(VERSION)
+        .about("Deploy with Nix and manage resources declaratively")
+}
 
 fn main() {
-    let x = nixops4_core::add(2, 2);
-    println!("add(2, 2) = {}", x);
+    let matches = root_command().get_matches();
+    match matches.subcommand() {
+        Some((name, _)) => {
+            eprintln!("nixops4 internal error: unknown subcommand: {}", name);
+            exit(1);
+        }
+        None => {
+            root_command().print_help().unwrap();
+            eprintln!("\nNo subcommand given");
+            exit(1);
+        }
+    }
 }
