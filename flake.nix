b72191f8c1669bc8df242e8386014363d096fad4
Robert Hensing <robert@roberthensing.nl>
2024-05-01 17:04:12 +0200

    dev: Add debug symbols to shell

--- flake.nix
+++ flake.nix
@@ -40,6 +40,15 @@
               LIBCLANG_PATH
               BINDGEN_EXTRA_CLANG_ARGS
               ;
+            NIX_DEBUG_INFO_DIRS =
+              let
+                # TODO: add to Nixpkgs lib
+                getDebug = pkg:
+                  if pkg?debug then pkg.debug
+                  else if pkg?lib then pkg.lib
+                  else pkg;
+              in
+              "${getDebug config.packages.nix}/lib/debug";
             buildInputs = [
               config.packages.nix
             ];
