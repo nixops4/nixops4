1fbfa5a68ec29bd6532a8ced612b257a9dd5c03c
Robert Hensing <robert@roberthensing.nl>
2024-05-23 13:50:06 +0200

    WIP 2

--- rust/nixops4/src/main.rs
+++ rust/nixops4/src/main.rs
@@ -4,7 +4,9 @@ use nixops4_core;
 use anyhow::Result;
 use clap::Command;
 use eval_client::EvalClient;
-use nixops4_core::eval_api::{AssignRequest, EvalRequest, FlakeRequest, SimpleRequest};
+use nixops4_core::eval_api::{
+    AssignRequest, EvalRequest, FlakeRequest, FlakeType, Id, SimpleRequest,
+};
 use std::process::exit;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -18,40 +20,45 @@ fn root_command() -> Command {
         )
 }
 
+/** Convenience function that sets up an evaluator with a flake, asynchronously with regard to evaluation. */
+fn with_flake<T>(f: impl FnOnce(&mut EvalClient, Id<FlakeType>) -> Result<T>) -> Result<T> {
+    EvalClient::with(|mut c| {
+        let flake_id = c.next_id();
+        // TODO: use better file path string type more
+        let cwd = std::env::current_dir()
+            .unwrap()
+            .to_string_lossy()
+            .to_string();
+        c.send(&EvalRequest::LoadFlake(AssignRequest {
+            assign_to: flake_id,
+            payload: FlakeRequest { abspath: cwd },
+        }))?;
+        f(&mut c, flake_id)
+    })
+}
+
 fn main() {
     let matches = root_command().get_matches();
     let r: Result<()> = match matches.subcommand() {
         Some(("deployments", sub_matches)) => {
             match sub_matches.subcommand() {
-                Some(("list", _)) => {
-                    EvalClient::with(|mut c| {
-                        let flake_id = c.next_id();
-                        let deployments_id = c.next_id();
-                        // TODO: use better file path string type more
-                        let cwd = std::env::current_dir()
-                            .unwrap()
-                            .to_string_lossy()
-                            .to_string();
-                        c.send(&EvalRequest::LoadFlake(AssignRequest {
-                            assign_to: flake_id,
-                            payload: FlakeRequest { abspath: cwd },
-                        }))?;
-                        c.send(&EvalRequest::ListDeployments(SimpleRequest {
-                            assign_to: deployments_id,
-                            payload: flake_id,
-                        }))?;
-                        let deployments = c.receive_until(|client| {
-                            client.check_error(flake_id)?;
-                            client.check_error(deployments_id)?;
-                            let x = client.get_deployments(flake_id);
-                            Ok(x.map(|x| x.clone()))
-                        })?;
-                        for d in deployments {
-                            println!("{}", d);
-                        }
-                        Ok(())
-                    })
-                }
+                Some(("list", _)) => with_flake(|mut c, flake_id| {
+                    let deployments_id = c.next_id();
+                    c.send(&EvalRequest::ListDeployments(SimpleRequest {
+                        assign_to: deployments_id,
+                        payload: flake_id,
+                    }))?;
+                    let deployments = c.receive_until(|client| {
+                        client.check_error(flake_id)?;
+                        client.check_error(deployments_id)?;
+                        let x = client.get_deployments(flake_id);
+                        Ok(x.map(|x| x.clone()))
+                    })?;
+                    for d in deployments {
+                        println!("{}", d);
+                    }
+                    Ok(())
+                }),
                 Some((name, _)) => {
                     eprintln!("nixops4 internal error: unknown subcommand: {}", name);
                     exit(1);
