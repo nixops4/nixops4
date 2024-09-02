24284720a41e46b3150f5c76ee59a749b2927435
Robert Hensing <robert@roberthensing.nl>
2024-09-02 21:37:10 +0200

    test: Cross-check the resource message examples against the schema

--- test/json-schema.nix
+++ test/json-schema.nix
@@ -8,6 +8,8 @@ runCommand "check-schemas"
     (
       set -x;
       jv http://json-schema.org/draft-04/schema# ${../rust/nixops4-resource/resource-schema-v0.json}
+      jv ${../rust/nixops4-resource/resource-schema-v0.json}#/definitions/CreateResourceRequest ${../rust/nixops4-resource/examples/v0/CreateResourceRequest.json}
+      jv ${../rust/nixops4-resource/resource-schema-v0.json}#/definitions/CreateResourceResponse ${../rust/nixops4-resource/examples/v0/CreateResourceResponse.json}
     )
     touch $out
   ''
