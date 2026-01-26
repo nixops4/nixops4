7155a36ef82f13d68b2e1a933ad3d982c512d764
Hercules CI Effects <hercules-ci[bot]@users.noreply.github.com>
2026-01-26 02:05:41 +0000

    Update manual/development
    Store path: /nix/store/mcs9k2i2qhx74khrjrdqm8n7ri3k5wr9-nixops-manual/share/doc/nixops4/manual/html

--- manual/development/searcher-c2a407aa.js
+++ manual/development/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2cd12235.js',
+                path_to_root + 'searchindex-4ffa5b2f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
